7d3ad42eeba4888b9680b61e9424b1e672535e27
Taylor Martin <taylormartind@gmail.com>
2025-02-09 16:19:44 -0500

    added navbar component to dashboard screen

--- Growthly/app/screens/DashboardScreen.tsx
+++ Growthly/app/screens/DashboardScreen.tsx
@@ -1,3 +1,15 @@
-export default function DashboardScreen(){
-    
-}
+import React from 'react';
+import { View, Text} from 'react-native';
+import styles from '@/styles/dashboard-styles';
+import NavBar from '@/app/components/NavBar/NavBar';
+
+export default function DashboardScreen() {
+  return (
+    <View style={styles.screenContainer}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.headerText}>Dashboard Screen</Text>
+      </View>
+      <NavBar />
+    </View>
+  );
+};
