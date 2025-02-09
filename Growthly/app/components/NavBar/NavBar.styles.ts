ce04470d0064fa08b9652e394d569c72b8fd3cc3
Taylor Martin <taylormartind@gmail.com>
2025-02-09 12:39:47 -0500

    styles for navbar component

---
+++ Growthly/app/components/NavBar/NavBar.styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from '@/styles/colors';
+
+const styles = StyleSheet.create({
+    navBar: {
+        backgroundColor: Colors.growthly_white,
+        height: 131
+    },
+    navBarIcons: {
+        width: 32,
+        height: 32
+    },
+});
+
+export default styles;
