818243aa8f1199ae0923e64b15319dd8a43185b6
Taylor Martin <taylormartind@gmail.com>
2025-02-20 23:43:35 -0500

    built out loan application screen, bill cycle still needs btn states

--- Growthly/app/screens/LoansScreen.tsx
+++ Growthly/app/screens/LoansScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 import { useUser } from '../../context/UserContext';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
+import { Colors } from '@/styles/colors';
 import styles from '@/styles/loan-screen-styles';
 import Header from '@/app/components/Header/Header';
 import NavBar from '@/app/components/NavBar/NavBar';
@@ -10,29 +11,49 @@ import NavBar from '@/app/components/NavBar/NavBar';
 export default function LoansScreen() {
 
     const { user } = useUser();
-    const router = useRouter();
     const [currentStep, setCurrentStep] = useState(1);
+    const [selectedDuration, setSelectedDuration] = useState<number | null>(null);
+    const duration: Array<1 | 3 | 6 | 9 | 12> = [1, 3, 6, 9, 12];
+    const positionStyles: { [key in 1 | 3 | 6 | 9 | 12]: { left: number } } = {
+        1: { left: 20 },
+        3: { left: 88.75 },
+        6: { left: 157.5 },
+        9: { left: 226.5 },
+        12: { left: 295 },
+    };    
 
     // hooks for switching between apply and manage
     const handleNextStep = () => {
         setCurrentStep(currentStep + 1);
     };
-
+    // goes back in the steps so user can toggle between apply and manage
     const handleBackStep = () => {
         setCurrentStep(currentStep - 1);
     }
 
-    // manage
+    // radio button hook
+    const handleDurationSelect = (duration: number) => {
+        setSelectedDuration(duration);
+    };
 
     return (
         <View style={styles.screenContainer}>
             <Header 
-            title={`${user?.first_name || 'Guest'}, you have 0 active loan(s). `}
-            subtitle='Apply for loans and manage them below.'
+                title={`${user?.first_name || 'Guest'}, you have 0 active loan(s). `}
+                subtitle='Apply for loans and manage them below.'
             />
 
+            {/* step 1 renders apply for loan tab */}
             { currentStep === 1 ? (
             <>
+                <Text style={styles.headerText}>Apply for Loan</Text>
+                <View style={styles.contentContainer}>
+                    <Text style={{ fontWeight: 'bold' }}>Warning:</Text>
+                    <Text>Active loans limit is: 2 - Total loan limit is: $2500</Text>
+                    <Text>Fixed Monthly Interest: Determined by Match</Text>
+                </View>
+
+                {/* tabs */}
                 <TouchableOpacity style={styles.applyButtonActive}>
                     <Text style={styles.buttonText}>APPLY</Text>
                 </TouchableOpacity>
@@ -41,13 +62,75 @@ export default function LoansScreen() {
                     <Text style={styles.buttonText}>MANAGE</Text>
                 </TouchableOpacity>
 
-                <Text style={styles.test}>on step 1</Text>
+                {/* application container */}
+                <View style={styles.applyContainer}>
+                    {/* loan total section */}
+                    <View>
+                        <Text style={[styles.heading, styles.h1]}>Enter Total for Loan:</Text>
+                        <TouchableOpacity style={[styles.inputContainer, styles.fieldContainer1]}>
+                            <TextInput 
+                                placeholder='$0.00'
+                                textAlign='left'
+                                placeholderTextColor={Colors.growthly_darkblue}
+                                textAlignVertical='center'
+                                style={styles.inputField}
+                            />
+                        </TouchableOpacity>
+                        <View style={styles.keyline} />
+                    </View>
+
+                    {/* billing cycle section */}
+                    <View>
+                        <Text style={[styles.heading, styles.h2]}>Select Billing Cycle:</Text>
+                        <View>
+                            <TouchableOpacity style={styles.monthlyButton}>
+                                <Text style={styles.cycleButtonText}>Monthly</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <TouchableOpacity style={styles.biweeklyButton}>
+                                <Text style={styles.cycleButtonText}>Biweekly</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={[styles.keyline, styles.keyline2]} />
+                    </View>
+
+                    {/* Payback period section */}
+                    <View>
+                        <Text style={[styles.heading, styles.h3]}>Select Payback Duration:</Text>
+                        {/* Duration Buttons */}
+                        {duration.map((dur) => (
+                            <TouchableOpacity
+                                key={dur}
+                                style={[
+                                    styles.durationButton,
+                                    positionStyles[dur],
+                                    { top: 105 },
+                                    selectedDuration === dur ? { backgroundColor: Colors.growthly_green } : {},
+                                ]}
+                                onPress={() => handleDurationSelect(dur)}
+                            >
+                                <Text style={styles.cycleButtonText}>{dur}</Text>
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+                    <View style={[styles.keyline, styles.keyline3]} />
+
+                    {/* apply button */}
+                    <View>
+                        <TouchableOpacity style={styles.applyButton}>
+                            <Text style={styles.applyButtonText}>Apply</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </>
             ) : null}
 
-            {/* Step 2 of Form */}
+            {/* step 2 renders manage loan tab */}
             { currentStep === 2 ? (
             <>
+                <Text style={styles.headerText}>Manage Loan</Text>
+
                 <TouchableOpacity style={styles.applyButtonDisabled} onPress={handleBackStep}>
                     <Text style={styles.buttonText}>APPLY</Text>
                 </TouchableOpacity>
@@ -55,14 +138,9 @@ export default function LoansScreen() {
                 <TouchableOpacity style={styles.manageButtonActive}>
                     <Text style={styles.buttonText}>MANAGE</Text>
                 </TouchableOpacity>
-
-                <Text style={styles.test}>on step 2</Text>
             </>
             ) : null}
 
-            <View style={styles.contentContainer}>
-                <Text style={styles.headerText}>Loans Screen</Text>
-            </View>
             <NavBar />
         </View>
     );
--- Growthly/styles/loan-screen-styles.ts
+++ Growthly/styles/loan-screen-styles.ts
@@ -5,11 +5,13 @@ const styles = StyleSheet.create({
     screenContainer: {
         flex: 1,
     },
+
     contentContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        color: Colors.growthly_darkblue,
+        left: 40,
+        top: 240
     },
+
     headerText: {
         left: 40,
         top: 225,
@@ -17,6 +19,7 @@ const styles = StyleSheet.create({
         fontSize: 35,
         fontWeight: 'bold',
     },
+
     applyButtonActive: {
         width: 130,
         paddingVertical: 15,
@@ -25,14 +28,14 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 145,
         left: 40,
-        borderWidth: 1,
+        borderWidth: .75,
         borderColor: Colors.growthly_darkblue,
         backgroundColor: Colors.growthly_green,
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
     },
-    
+
     applyButtonDisabled: {
         width: 130,
         paddingVertical: 15,
@@ -41,14 +44,14 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 145,
         left: 40,
-        borderWidth: 1,
+        borderWidth: .750 ,
         borderColor: Colors.growthly_darkblue,
         backgroundColor: Colors.growthly_white,
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
     },
-    
+
     manageButtonActive: {
         width: 130,
         paddingVertical: 15,
@@ -64,7 +67,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'row',
     },
-    
+
     manageButtonDisabled: {
         width: 130,
         paddingVertical: 15,
@@ -80,16 +83,149 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'row',
     },
-    
+
     buttonText: {
         color: Colors.growthly_darkblue,
         fontFamily: 'Inter-Medium',
         fontSize: 15
     },
 
-    test: {
+    applyContainer: {
         left: 40,
-        top: 200
+        top: 265,
+        width: 350,
+        height: 410,
+        borderRadius: 15,
+        backgroundColor: Colors.growthly_lightblue,
+    },
+
+    keyline: {
+        position: 'absolute',
+        top: 118,
+        left: 20,
+        width: 310,
+        borderWidth: .25,
+        borderColor: Colors.growthly_teal2,
+      },
+
+    keyline2: {
+        top: 140,
+    },
+
+    keyline3: {
+        top: 327,
+    },
+
+    heading: {
+        fontFamily: 'Inter-Bold',
+        fontSize: 16,
+        color: Colors.growthly_darkblue
+    },
+
+    h1: {
+        left: 20,
+        top: 20,
+    },
+
+    h2: {
+        left: 20,
+        top: 45,
+        marginBottom: 55
+    },
+
+    h3: {
+        left: 20,
+        top: 80,
+        marginBottom: 55
+    },
+
+    inputContainer: {
+        height: 45,
+        borderRadius: 10,
+        marginBottom: 55,
+        backgroundColor: Colors.growthly_white
+    },
+
+    fieldContainer1: {
+        left: 20,
+        top: 35,
+        width: 310,
+        marginBottom: 25
+    },
+
+    inputField: {
+        color: Colors.growthly_darkblue,
+        height: 45,
+        width: '100%',
+        borderRadius: 10,
+        backgroundColor: Colors.growthly_white,
+        paddingLeft: 20,
+        fontSize: 16,
+        textAlignVertical: 'center'
+    },
+
+    monthlyButton: {
+        position: 'absolute',
+        top: 0,
+        left: 20,
+        width: 145,
+        height: 45,
+        marginTop: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: Colors.growthly_white
+    },
+
+    biweeklyButton: {
+        position: 'absolute',
+        top: 0,
+        left: 185,
+        width: 145,
+        height: 45,
+        marginTop: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: Colors.growthly_white
+    },
+
+    durationButton: {
+        position: 'absolute',
+        top: 35,
+        width: 35,
+        height: 35,
+        marginTop: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: Colors.growthly_white
+    },
+
+    cycleButtonText: {
+        fontSize: 16,
+        fontFamily: 'Inter-Regular',
+        color: Colors.growthly_darkblue
+    },
+
+    applyButton: {
+        position: 'absolute',
+        top: 100,
+        width: 310,
+        height: 45,
+        left: 20,
+        marginTop: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: Colors.growthly_darkblue
+
+    },
+
+    applyButtonText: {
+        fontSize: 16,
+        fontFamily: 'Inter-Bold',
+        color: Colors.growthly_white
     }
 });
 
