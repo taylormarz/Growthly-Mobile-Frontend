593d6f27718763ea45421ce00edeaed3437b22b0
Taylor Martin <taylormartind@gmail.com>
2025-03-18 17:24:23 -0400

    removed VIEW import not being used in halfinput component

--- Growthly/app/components/HalfInput/HalfInput.tsx
+++ Growthly/app/components/HalfInput/HalfInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Colors } from '@/styles/ColorPalette/colors';
 import styles from './HalfInput-styles';
 
